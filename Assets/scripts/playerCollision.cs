e88aa155314137ea6ee693f64fbe65e93700cba4
MD Kawsar Ahmed Jami <jamisays@gmail.com>
2019-01-04 20:45:38 +0600

    initial commit

---
+++ Assets/scripts/playerCollision.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+
+    public playerMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("hit obstacle");
+            movement.enabled = false;
+            FindObjectOfType<gameManager>().EndGame();
+        }
+    }
+
+}
+
---
+++ Assets/scripts/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewayForce = 600f;
+
+    // Update is called once per frame
+    void Update() {
+        rb.useGravity = true;
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<gameManager>().EndGame();
+        }
+    }
+}
